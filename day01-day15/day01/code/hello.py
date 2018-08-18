31520b5e4550875f55be88703717c502cbd45895
GhostDog <i@szpinc.org>
2018-08-18 16:04:43 +0800

    初识Python

---
+++ day01-day15/day01/code/hello.py
@@ -0,0 +1,17 @@
+'''
+第一个Python程序 - hello, world!
+向伟大的Dennis M. Ritchie先生致敬
+
+Version: 1.0
+Author: Ghost Dog
+Date: 2018-04-16
+
+请将文件命名为hello.py并在终端通过下面的命令运行它
+
+python hello.py
+'''
+
+print("Hello World!")
+print("你好，世界")
+print('Hello','World',sep=',', end='!')
+print("GoodBye,World",end='!\n')
